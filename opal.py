0a4d32810add467b94e28d269fa7eb8ddae9a059
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2019-03-12 23:12:42 +1100

    werkwerkwerk

--- opal.py
+++ opal.py
@@ -1,6 +1,7 @@
 import requests
 from json.decoder import JSONDecodeError
 import time
+import lxml.html as lh
 
 BASE_URL = 'https://www.opal.com.au'
 
@@ -40,31 +41,57 @@ class Opal():
         r = self.session.get(url, params=params)
         return r.json() # todo: add some validation
     
-    def _next_page(page):
-        return 2 # next page number or None
+    def _transactions(self, page):
+        def get_text(s):
+            if s is None:
+                return None
+            return s.strip()
+
+        def get_mode(x):
+            try:
+                return x[0].get('alt', None)#.values()
+            except:
+                return None
+
+        doc = lh.fromstring(page)
+        for x in doc.xpath('//tbody/tr'): # for each transaction
+            yield {
+                'tx_id': int(get_text(x[0].text)),
+                'date': get_text(x[1].text_content()),
+                'mode': get_mode(x[2]),
+                'details': get_text(x[3].text),
+                'journey_number': get_text(x[4].text),
+                'fare_applied': get_text(x[5].text),
+                'fare': get_text(x[6].text),
+                'discount': get_text(x[7].text),
+                'amount': get_text(x[8].text)
+            }
     
-    def _transactions(page):
-        # tx_number, date, mode, details, journey number, fare applied, fare, discount, amount
-        yield (1) 
-    
-    def trips(self, card=0, up_to_tx=None, page=1, limit=None):
+    def trips(self, card=2, up_to_tx=None, page=1, limit=-1):
         if self.session is None:
             raise RuntimeError('need to login first')
-        # get the data
 
-        # parse the stuff -> (next_page?, transaction numbers)
-        # if found the transaction number
-        r = 'do the request'
+        url = BASE_URL + '/registered/opal-card-activities-list'
+        params = {
+            "AMonth": "-1",
+            "AYear": "-1",
+            "cardIndex": card,
+            "pageIndex": page,
+            '_': int(time.time())
+        }
+        r = self.session.get(url, params=params)
+
         for t in self._transactions(r.text):
-            if t[0] <= limit:
+            if t['tx_id'] <= limit:
                 return
             yield t
-        next_page = self._next_page(r.text)
-        if next_page is None:
+        if 'title="Next page"' not in r.text:
             return
-        self.trips(card, up_to_tx, page+1, limit)
+        yield from self.trips(card, up_to_tx, page+1, limit)
 
 
 o = Opal()
 print(o.login('email@here.com', 'passw0rd'))
 print(o.cards())
+for t in o.trips():
+    print(t)
